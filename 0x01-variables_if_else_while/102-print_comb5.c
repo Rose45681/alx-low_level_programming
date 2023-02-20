e881bd2687bd8eed736ec3d5a8de328252cc8d32
Rose45681 <mathebularose534@gmail.com>
2023-02-20 07:22:40 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 / 10) + '0');
-			putchar('');
+			putchar(',');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
@@ -25,7 +25,7 @@ int main(void)
 				continue;
 
 			putchar(',');
-			putchar('');
+			putchar(',');
 		}
 	}
 
