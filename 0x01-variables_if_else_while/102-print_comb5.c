3ebcf95f9d62f7c8de0b9af131550ee96e0cab4e
Rose45681 <mathebularose534@gmail.com>
2023-02-20 08:06:32 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 00 98; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 + 00 01; num2 <= 00 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((num1 / 00 10) + '0');
-			putchar((num1 / 00 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 / 10) + '0');
 			putchar(',');
-			putchar((num2 / 00 10) + '0');
-			putchar((num2 % 00 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (num1 == 00 98 && num2 == 00 99)
+			if (num1 == 98 && num2 == 99)
 
 				continue;
 
